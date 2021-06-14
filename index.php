e79822e9b3f6f58c4ce862f83dc6d6ff7ae3c555
will <2017730059@student.unpar.ac.id>
2021-06-14 18:35:57 +0700

    tmbh view footer header biasa

--- index.php
+++ index.php
@@ -2,15 +2,6 @@
     // dalam View atau disini aja?
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include('view/header.php') ?>
+<h1>Delivery Service</h1>
+<?php include('view/footer.php') ?>
