fa87634b0902265c0e518b495d6252f4dc8c7646
will <2017730059@student.unpar.ac.id>
2021-06-15 15:37:34 +0700

    nambah page signup.php

--- index.php
+++ index.php
@@ -2,15 +2,8 @@
     // dalam View atau disini aja?
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include('view/header.php') ?>
+
+<?php include('view/signup_page.php') ?>
+
+<?php include('view/footer.php') ?>
---
+++ view/signup_page.php
@@ -0,0 +1,19 @@
+<?php include('view/header.php') ?>
+<h1>Sign Up</h1>
+<hr>
+
+<form action="" method="post">
+    <label for="">Name:</label>
+    <input type="text" name="name">
+    
+    <label for="">E-mail:</label>
+    <input type="text" name="email">
+    
+    <label for="">Password:</label>
+    <input type="password" name="pass">
+    
+    <label for="">Re-enter Password:</label>
+    <input type="password" name="pass">
+    
+</form>
+<?php include('view/footer.php') ?>
